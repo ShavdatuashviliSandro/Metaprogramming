71c065a2f9c3e4840368d2b2fc05f649580b0815
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2024-01-23 12:16:01 +0400

    singleton $ instance methods

--- main.rb
+++ main.rb
@@ -1,16 +1,17 @@
 class MyClass
   def instance_method
-    @@class_variable = 'I am a class variable'
-    @@class_instance_variable = 'I am a class instance variable'
+    @class_variable = 'I am a class variable'
+    @class_instance_variable = 'I am a class instance variable'
   end
 
   def self.class_method
-    puts @@class_variable # This works
-    puts @@class_instance_variable # This works
+    puts @class_variable
+    puts @class_instance_variable
   end
 end
+
+MyClass.class_method
 
 obj = MyClass.new
 obj.instance_method
 
-MyClass.class_method
